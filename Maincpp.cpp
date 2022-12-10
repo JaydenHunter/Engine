30cab1aed314f823c0adb95dd5f60cb00b6f03c8
JaydenHunter <jaydenghunter@outlook.com>
2022-12-10 22:05:18 +1100

    First temporary shaders and default vertex buffer data

--- Maincpp.cpp
+++ Maincpp.cpp
@@ -1,6 +1,33 @@
 #include <glad/glad.h>
 #include <glfw3.h>
 #include <iostream>
+#include <crtdbg.h>
+
+//Temporary Vertex Shader
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
+"}\0";
+
+//Temporary Fragment Shader
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"FragColor = vec4(1.0f,0.5f,0.2f,1.0f);\n"
+"}\0";
+
+
+
+//Temporary
+float vertices[] =
+{
+	-0.5f,-0.5f,0.0f,
+	0.5f,-0.5f,0.0f,
+	0.0f,0.5f,0.0f
+};
 
 // Callback for when the user resizes the viewport
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -8,8 +35,17 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
+	// Flag to check for memory leaks
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	// Initialize GLFW with version 3.3 in core profile.
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -47,9 +83,95 @@ int main()
 	//Register callback event
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	//Testing Code to generate a vertex buffer
+
+	//Vertex Buffer Object
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// Copy the vertex data into the buffer's memory
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Create our vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// Check if shader compiled correctly 
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	else
+	{
+		std::cout << "SUCCESS::SHADER::VERTEX::COMPILATION_SUCCESS" << std::endl;
+	}
+
+	// Create our fragment shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// Check if fragment shader compiled correctly
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	else
+	{
+		std::cout << "SUCCESS::SHADER::FRAGMENT::COMPILATION_SUCCESS" << std::endl;
+	}
+
+	// Link our shaders to a shader program which will be used when we issue render calls
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// After linking, we should delete our shaders
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	// Check if the shader program was successful
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER_PROGRAM::LINKING_FAILED" << std::endl;
+	}
+	else
+	{
+		std::cout << "SUCCESS::SHADER_PROGRAM::LINKING_SUCCESS" << std::endl;
+	}
+
+	// Every shader and rendering call after glUseProgram will now use this program object (shaders).
+	glUseProgram(shaderProgram);
+
+
 	// Render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// Clear the screen
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Handle inputs
+		processInput(window);
+
+		// Rendering commands go here
+		//....
+
+
+		// Check and call events and swap the buffers.
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
