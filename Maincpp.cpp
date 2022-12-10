195ae995f1d7b91a3b18ffb7eafbf7f4afdc13f0
JaydenHunter <jaydenghunter@outlook.com>
2022-12-10 21:17:53 +1100

    Initial Window working

---
+++ Maincpp.cpp
@@ -0,0 +1,60 @@
+#include <glad/glad.h>
+#include <glfw3.h>
+#include <iostream>
+
+// Callback for when the user resizes the viewport
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+int main()
+{
+	// Initialize GLFW with version 3.3 in core profile.
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a window object
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLAD.
+	// pass GLAD the function to load the address of the OpenGL Function pointers.
+	// glfwGetProcAddress that defines the correct function based on which OS we're compiling for.
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Inform OpenGL the size of the renderering window so OpenGL knows how we want to display the data nd co-ords with respect
+	// to the window
+	// First two params are location
+	// Third and fourth are width and height
+	// Setting the viewport dimensions smaller than GLFW's dimensions would make all OpenGL rendering be displayed in a smaller window
+	// and we could for example display other elements outside of the OpenGL Viewport.
+	// Processed coordiants in OpenGL are between -1 and 1.
+	glViewport(0, 0, 800, 600);
+
+	//Register callback event
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
