e2570fc2054e32d97e67cf48b1a00fd02e0d3229
JaydenHunter <jaydenghunter@outlook.com>
2022-12-11 18:01:26 +1100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -264,8 +264,8 @@ int main()
 		processInput(window);
 
 		// Model Matrix
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::rotate(model,(float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+		/*glm::mat4 model = glm::mat4(1.0f);
+		model = glm::rotate(model,(float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));*/
 
 		// View Matrix
 		glm::mat4 view = glm::mat4(1.0f);
@@ -276,8 +276,7 @@ int main()
 		glm::mat4 projection;
 		projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 
-		int modelLoc = glGetUniformLocation(ourFirstShader.ID, "model");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		
 
 		int viewLoc = glGetUniformLocation(ourFirstShader.ID, "view");
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
@@ -291,7 +290,17 @@ int main()
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);;
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		//glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f + angle), glm::vec3(0.5f, 1.0f, 0.0f));
+			int modelLoc = glGetUniformLocation(ourFirstShader.ID, "model");
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		// Check and call events and swap the buffers.
 		glfwSwapBuffers(window);
 		glfwPollEvents();
